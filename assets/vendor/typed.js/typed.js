0fc4a0f2f6a53d04063a05fdbef82354a6e0ca16
svperion <32706652+svperion@users.noreply.github.com>
2022-11-26 19:50:36 -0500

    Modified more

--- assets/vendor/typed.js/typed.js
+++ assets/vendor/typed.js/typed.js
@@ -831,7 +831,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  /**
 	   * @property {number} backDelay time before backspacing in milliseconds
 	   */
-	  backDelay: 700,
+	  backDelay: 1200,
 	
 	  /**
 	   * @property {boolean} fadeOut Fade out instead of backspace
